256ae948fadc03677721ac33eda7f9774f9ce0cc
Ray Clanan <rclanan@utopianconcept.com>
2016-05-18 10:12:55 -0400

    Add cors back

--- server/app.js
+++ server/app.js
@@ -3,11 +3,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes/index');
+const cors = require('cors');
 
 let app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+// TODO: secure me
+app.use(cors());
 
 app.use(routes);
 
