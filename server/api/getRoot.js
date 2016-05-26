778c52a761d6255e1f08f7ba48484396ef8489bb
brian wortman <bwortman@fusionalliance.com>
2016-05-26 09:56:24 -0400

    Completed Hello World scenario.

--- server/api/getRoot.js
+++ server/api/getRoot.js
@@ -1,17 +1,22 @@
 'use strict';
 
 const express = require('express');
+const models = require('../models');
 const pkgJSON = require('../../package.json');
 
 function getRoot(request, response) {
-  let app = express();
-  let env = app.get('env');
 
-  response.status(200).json({
-    title: pkgJSON.description,
-    environment: env,
-    version: pkgJSON.version
-  });
+  models.User.findOne({ where: {username: 'newton'} }).then(function(user) {
+    let app = express();
+    let env = app.get('env');
+
+    response.status(200).json({
+      user: user.username,
+      title: pkgJSON.description,
+      environment: env,
+      version: pkgJSON.version
+    });
+  })
 }
 
 module.exports = getRoot;
