0f57ada4f8917e62cdf7964f5c63dcdf48bfec62
Oza <doza@fusionalliance.com>
2016-06-03 17:04:51 -0400

    info added

--- server/api/index.js
+++ server/api/index.js
@@ -3,9 +3,10 @@
 
 const getRoot = require('./getRoot');
 const log = require('./log');
+log('Auro Renter','Info','Api request begin');
 module.exports = {
   getRoot
 };
 // TO DO : It should be username 
-log('Auro Renter','Info','Api requersted');
 
+log('Auro Renter','Info','Api request end');
