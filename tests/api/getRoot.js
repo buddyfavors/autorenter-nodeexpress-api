2a35dcde6bcfdf0497408b5a4bd44ae296ab3773
Ray Clanan <rclanan@utopianconcept.com>
2016-06-14 16:42:45 -0400

    Pass err into done

--- tests/api/getRoot.js
+++ tests/api/getRoot.js
@@ -10,6 +10,7 @@ describe('(api root) /', () => {
       .set('Accept', 'application/json')
       .expect(200)
       .end((err, res) => {
+        if (err) done(err);
         expect(res.body).to.have.property('user');
         expect(res.body.user).to.not.equal(null);
         expect(res.body).to.have.property('title');
@@ -18,7 +19,6 @@ describe('(api root) /', () => {
         expect(res.body.environment).to.not.equal(null);
         expect(res.body).to.have.property('version');
         expect(res.body.version).to.not.equal(null);
-        if (err) return done(err); 
         done();
       });
   });
