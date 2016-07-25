fb1dab302df2509f684d3b4e4ce46030e94ad1e3
brian wortman <bwortman@fusionalliance.com>
2016-07-25 11:36:29 -0400

    Fixed path to parallelshell in nodemon.json Factored the body of app.js into some cohesive functions.

--- server/app.js
+++ server/app.js
@@ -7,16 +7,23 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+function configureBodyParser() {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+}
 
-// TODO: secure me
-app.use(cors({
-  origin: ['http://192.168.99.100:3000', 'http://192.168.99.100'],
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type'],
-  preflightContinue: true
-}));
+function configureCors() {
+  // TODO: secure me
+  app.use(cors({
+    origin: ['http://192.168.99.100:3000', 'http://192.168.99.100'],
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
+    preflightContinue: true
+  }));
+}
+
+configureBodyParser();
+configureCors();
 
 app.use(routes);
 
