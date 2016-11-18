e99ff0aafe9d566cab8dafc5a602cd3f6736ce66
Andrew McConnell <amcconnell@fusionalliance.com>
2016-11-17 21:39:40 -0500

    Freeze PostgreSQL container version at 9.5.

--- bin/build-server.sh
+++ bin/build-server.sh
@@ -5,7 +5,7 @@
 DB_ID=$(docker ps -a --filter=name=aur-db -q)
 if [ -z $DB_ID ]
 then
-  echo '=> Starting db' && docker run -d -p 5432:5432 --name aur-db -e POSTGRES_PASSWORD=postgres -v postgres_data:/var/lib/postgresql/data --restart=always postgres
+  echo '=> Starting db' && docker run -d -p 5432:5432 --name aur-db -e POSTGRES_PASSWORD=postgres -v postgres_data:/var/lib/postgresql/data --restart=always postgres:9.5
 else
   echo '=> Found non-running postgresql container with id $DB_ID'
   echo '=> Restarting db' && docker start $DB_ID
