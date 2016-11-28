450863928fbb3f136436ef10cd2015e06e6dcdb6
brian wortman <bwortman@fusionalliance.com>
2016-11-28 14:51:38 -0500

    Simplified by removing the promise variable. Should have done this in the last checkin, where I combined the 2 tests.

--- tests/api/raiseError.js
+++ tests/api/raiseError.js
@@ -8,12 +8,10 @@ chai.should();
 
 it('/api/raise-error should return correct response', () => {
   const expectedMessage = 'Error: An API-originated error for testing purposes.';
-  const thePromise = chai
+  return chai
     .request(app)
     .get('/raise-error')
-    .set('Accept', 'application/json');
-
-  return thePromise
+    .set('Accept', 'application/json')
     .then((res) => { throw res; })
     .catch((err) => {
       const actualMessage = err.response.error.text.substring(0, expectedMessage.length);
