2dadd78c5600a69299669fa0aba4c2c942e4e671
Jacob King <jking@fusionalliance.com>
2016-12-02 14:22:17 -0500

    Created test files for the rest of the JsonFIles tests.

---
+++ tests/models/JsonFiles/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const assert = require('assert');
+
+describe('models/JsonFiles/index', () => {
+  it.skip('sets the root path for the models', () => {
+    assert(false);
+  });
+});
---
+++ tests/models/JsonFiles/removeDocument.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const assert = require('assert');
+const fs = require('fs');
+const removeDocument = require('../../../server/models/JsonFiles/removeDocument');
+const path = require('path');
+const sinon = require('sinon');
+
+describe('models/JsonFiles/removeDocument', () => {
+
+  const mockModel = {
+    pathname: 'sample/path/name',
+    removeDocument: removeDocument
+  };
+  const testObject = { a: 123, b: 456 };
+
+  let readFileSyncStub;
+  let unlinkSyncStub;
+
+  beforeEach(() => {
+    readFileSyncStub = sinon.stub(fs, 'readFileSync')
+      .returns(JSON.stringify(testObject));
+    unlinkSyncStub = sinon.stub(fs, 'unlinkSync')
+      .returns(JSON.stringify(testObject));
+  });
+
+  afterEach(() => unlinkSyncStub.restore());
+
+  it.skip('removes a document', () => {
+
+  });
+});
