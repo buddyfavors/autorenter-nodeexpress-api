6923b6e301362ed5f45b60f2be7d59ac4eacd91f
Ray Clanan <rclanan@utopianconcept.com>
2016-12-02 12:26:41 -0500

    Fix issue with config having the wrong file path.

--- server/config.js
+++ server/config.js
@@ -6,5 +6,5 @@ const path = require('path');
 config.server = {
   host: process.env.HOST || '0.0.0.0',
   port: process.env.PORT || 3000,
-  jsonFilesBasePath: path.resolve(__dirname, './server/data'),
+  jsonFilesBasePath: path.resolve(__dirname, './data'),
 };
