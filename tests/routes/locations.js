bdb13e1a99aac48cdd3337e4ade819ab5c2508cd
DESKTOP-QCLVIHN\brian <bwortman@fusionalliance.com>
2016-12-12 21:25:49 -0500

    Fixed broken locations tests. Removed raiseError from under the logging route.

--- tests/routes/locations.js
+++ tests/routes/locations.js
@@ -10,7 +10,7 @@ chai.use(chaiHttp);
 describe('routes/locations', () => {
   describe('locations', () => {
     it('should pull the json data from the model', () => {
-      let expectedOutput = {'data': [{ 'id': 'bbf412c4f921d8f23571faadd5bced930f9232ca', 'siteId': '13Z', 'name': 'Loring Seaplane Base', 'vehicles': '0', 'city': 'Loring', 'stateCode': 'AK' }]};
+      let expectedOutput = {'data': [{ 'id': 'bbf412c4f921d8f23571faadd5bced930f9232ca', 'siteId': '13Z', 'name': 'Loring Seaplane Base', 'vehicleCount': '0', 'city': 'Loring', 'stateCode': 'AK' }]};
 
       chai.request(server)
         .get('/api/locations')
@@ -22,7 +22,7 @@ describe('routes/locations', () => {
   });
   describe('location/:id', () => {
     it('should pull the json data from the model', () => {
-      let expectedOutput = {'data': { 'id': 'bbf412c4f921d8f23571faadd5bced930f9232ca', 'siteId': '13Z', 'name': 'Loring Seaplane Base', 'vehicles': '0', 'city': 'Loring', 'stateCode': 'AK' }};
+      let expectedOutput = {'data': { 'id': 'bbf412c4f921d8f23571faadd5bced930f9232ca', 'siteId': '13Z', 'name': 'Loring Seaplane Base', 'vehicleCount': '0', 'city': 'Loring', 'stateCode': 'AK' }};
 
       chai.request(server)
         .get('/api/locations/bbf412c4f921d8f23571faadd5bced930f9232ca')
