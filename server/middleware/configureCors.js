36becec3ca47842fb4e5ee9adb928a3127e250a1
Ray Clanan <rclanan@utopianconcept.com>
2016-12-13 16:15:55 -0500

    Update CORS to reflect the verbs and port of the UI

--- server/middleware/configureCors.js
+++ server/middleware/configureCors.js
@@ -4,8 +4,8 @@ const cors = require('cors');
 
 function configureCors(app) {
   app.use(cors({
-    origin: ['http://127.0.0.1:3000', 'http://127.0.0.1'],
-    methods: ['GET', 'POST'],
+    origin: ['http://127.0.0.1:3000', 'http://127.0.0.1:8080'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type'],
     preflightContinue: true
   }));
