d67bf40d0874c072e91bf260615a0f71c28685de
Pascucci <cpascucci@fusionalliance.com>
2017-02-01 13:56:15 -0500

    Updated Swagger documentation for Vehicle and Location routes.

--- server/routes/locations/index.js
+++ server/routes/locations/index.js
@@ -38,7 +38,96 @@ module.exports = router;
  *           $ref: '#/definitions/Location'
  */
 router.get('/locations', require('./getAllLocations'));
+
+/**
+ * @swagger
+ * /api/locations/{id}:
+ *   get:
+ *     tags:
+ *       - Locations
+ *     description: Returns a single location
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - locationId: id
+ *         description: Location's id
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: A single location
+ *         schema:
+ *           $ref: '#/definitions/Location'
+ */
 router.get('/locations/:id', require('./getLocation'));
+
+/**
+ * @swagger
+ * /api/locations:
+ *   post:
+ *     tags:
+ *       - Locations
+ *     description: Creates a new location
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: location
+ *         description: Location object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Location'
+ *     responses:
+ *       200:
+ *         description: Successfully created
+ */
 router.post('/locations', require('./postLocation'));
+
+/**
+ * @swagger
+ * /api/locations/{id}:
+ *   put:
+ *     tags:
+ *       - Locations
+ *     description: Updates a single location
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         description: Location's id
+ *         in: path
+ *         required: true
+ *         type: string
+ *       - name: location
+ *         in: body
+ *         description: Fields for the Location resource
+ *         schema:
+ *           type: array
+ *           $ref: '#/definitions/Location'
+ *     responses:
+ *       200:
+ *         description: Successfully updated
+ */
 router.put('/locations/:id', require('./putLocation'));
+
+/**
+ * @swagger
+ * /api/locations/{id}:
+ *   delete:
+ *     tags:
+ *       - Locations
+ *     description: Deletes a single location
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         description: Location's id
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Successfully deleted
+ */
 router.delete('/locations/:id', require('./deleteLocation'));
--- server/routes/vehicles/index.js
+++ server/routes/vehicles/index.js
@@ -32,22 +32,122 @@ module.exports = router;
 
 /**
  * @swagger
- * /api/locations/:locationId/vehicles:
+ * /api/locations/{locationId}/vehicles:
  *   get:
  *     tags:
  *       - Vehicles
  *     description: Returns all vehicles for a location
  *     produces:
  *       - application/json
+ *     parameters:
+ *       - locationId: locationId
+ *         description: Location's id
+ *         in: path
+ *         required: true
+ *         type: string
  *     responses:
  *       200:
  *         description: An array of vehicles
  *         schema:
  *           $ref: '#/definitions/Vehicle'
  */
-
 router.get('/locations/:locationId/vehicles', require('./getAllVehicles'));
+
+/**
+ * @swagger
+ * /api/vehicles/{id}:
+ *   get:
+ *     tags:
+ *       - Vehicles
+ *     description: Returns a single vehicle
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - vehicleId: id
+ *         description: Vehicle's id
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: A single vehicle
+ *         schema:
+ *           $ref: '#/definitions/Vehicle'
+ */
 router.get('/vehicles/:id', require('./getVehicle'));
+
+/**
+ * @swagger
+ * /api/locations/{locationId}/vehicles:
+ *   post:
+ *     tags:
+ *       - Vehicles
+ *     description: Creates a new vehicle
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - locationId: locationId
+ *         description: Location's id
+ *         in: path
+ *         required: true
+ *         type: string
+ *       - name: vehicle
+ *         description: Vehicle object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Vehicle'
+ *     responses:
+ *       200:
+ *         description: Successfully created
+ */
 router.post('/locations/:locationId/vehicles', require('./postVehicle'));
+
+
+/**
+ * @swagger
+ * /api/vehicles/{id}:
+ *   put:
+ *     tags:
+ *       - Vehicles
+ *     description: Updates a single vehicle
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         description: Vehicle's id
+ *         in: path
+ *         required: true
+ *         type: string
+ *       - name: vehicle
+ *         in: body
+ *         description: Fields for the Vehicle resource
+ *         schema:
+ *           type: array
+ *           $ref: '#/definitions/Vehicle'
+ *     responses:
+ *       200:
+ *         description: Successfully updated
+ */
 router.put('/vehicles/:id', require('./putVehicle'));
+
+/**
+ * @swagger
+ * /api/vehicles/{id}:
+ *   delete:
+ *     tags:
+ *       - Vehicles
+ *     description: Deletes a single vehicle
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         description: Vehicle's id
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Successfully deleted
+ */
 router.delete('/vehicles/:id', require('./deleteVehicle'));
