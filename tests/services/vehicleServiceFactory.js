9e974ad353753f73230276bfc8d18dc10522cd6e
BWortman <bwortman@fusionalliance.com>
2017-03-30 20:35:04 -0400

    Doing some TDD for an interview. Assignment is to fix failing test.

---
+++ tests/services/vehicleServiceFactory.js
@@ -0,0 +1,45 @@
+﻿'use strict';
+
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+
+const vehicleService = require('../../server/services/vehicleService');
+
+chai.should();
+chai.use(chaiAsPromised);
+
+describe('vehicleService', () => {
+  const locationId = 'c0b694ec-3352-43e3-9f22-77c87fe83d48';
+
+  describe('getVehicles', () => {
+    let vehiclesPromise;
+    beforeEach(() => {
+      vehiclesPromise = vehicleService.getVehicles(locationId);
+    });
+
+    it('returns vehicle data for specified location', () => {
+      return vehiclesPromise
+      .then((vehicles) => {
+        vehicles.should.have.length.of(2);
+        vehicles.every((vehicle) => {
+          return vehicle.locationId === locationId;
+        }).should.be.true;
+      })
+      .catch((err) => {
+        throw err;
+      });
+    });
+
+    it('returns vehicle data enriched with make and model', () => {
+      return vehiclesPromise
+        .then((vehicles) => {
+          vehicles.every((vehicle) => {
+            return vehicle.make && vehicle.model;
+          }).should.be.true;
+        })
+        .catch((err) => {
+          throw err;
+        });
+    });
+  });
+});
